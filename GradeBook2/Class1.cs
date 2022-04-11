d58bdb7961716ee198cd25158eb79e10a5db0039
josefpanek <josef.panek@gmail.com>
2022-04-11 23:18:19 +0200

    Add project files.

---
+++ GradeBook2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook2
+{
+    class Book
+    {
+        public void AddGrade(double grade)
+        {
+            Console.WriteLine();
+        }
+    }
+}
